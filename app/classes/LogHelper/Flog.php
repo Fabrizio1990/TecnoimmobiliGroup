338a6525d10a6f2139757c4a105051e2f1dc5902
Fabrizio1990 <fabriziocoppolecchia@gmail.com>
2016-12-27 12:49:21 +0100

    Now the log is stored by day and logs older than 7 days will be deleted

--- app/classes/LogHelper/Flog.php
+++ app/classes/LogHelper/Flog.php
@@ -6,6 +6,8 @@ class Flog{
 	//private static $log_path = $_SERVER['DOCUMENT_ROOT'];
 	private static $log_file_name;
 	private static $extension = ".txt";
+
+    private static $log_expire_time = 604800; //expire time in minutes, 7 days = 7*24*60*60
 	
 	
 	public static function logError($text,$log_file_name ="log"){
@@ -13,12 +15,13 @@ class Flog{
 		//self::$log_path = "D:/xampp/htdocs/Tecnoimmobili/SITE/log/";	
 		//PROD
 		//self::$log_path = dirname(__FILE__)."/../../log/";
-		
-		$date = Date("Y-m-d h:i:s");
+        //cancello i log vecchi
+        self::deleteOlderLog(self::$log_path);
+		$date  = Date("Y-m-d h:i:s");
 
 
 		$fullText =$date." <--> ". $text . " <--> " . self::getBacktrace() ;
-		self::$log_file_name = $log_file_name .self::$extension;
+		self::$log_file_name = Date("Y_m_d")."_".$log_file_name .self::$extension;
 		self::WriteFile($fullText);
 	}
 	
@@ -35,6 +38,39 @@ class Flog{
         $data = ob_get_clean();
         return $data;
     }
+
+
+
+    private function deleteOlderLog($path){
+
+
+        $tmpFolder=$path.'/';
+        $fileTypes="*.*";
+
+        foreach (glob($tmpFolder . $fileTypes) as $Filename) {
+
+            // Read file creation time
+            $FileCreationTime = filectime($Filename);
+
+            // Calculate file age in seconds
+            $FileAge = time() - $FileCreationTime;
+            echo("<br>FILEAGE = ".$FileAge);
+            echo("<br>log_expire_time = ".self::$log_expire_time);
+            // Is the file older than the given time span?
+            if ($FileAge > self::$log_expire_time){
+
+                // Now do something with the olders files...
+
+                echo "<br>The file $Filename is older than ".self::$log_expire_time." minutes\n";
+
+                //delete files:
+                unlink($Filename);
+            }
+
+        }
+    }
+
+
 
 
 }
