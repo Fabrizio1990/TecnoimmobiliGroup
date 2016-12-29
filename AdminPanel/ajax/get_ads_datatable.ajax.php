cb6b7e9ee06426633189a5f701e30d49a964d735
Fabrizio1990 <fabriziocoppolecchia@gmail.com>
2016-12-29 10:40:19 +0100

    implemented icon echo

--- AdminPanel/ajax/get_ads_datatable.ajax.php
+++ AdminPanel/ajax/get_ads_datatable.ajax.php
@@ -78,21 +78,21 @@
 
     if(isset($_GET["provincia"])){
         if($_GET["provincia"]!="") {
-            array_push($params, " id_city = in(?)");
+            array_push($params, " id_city  in(?)");
             array_push($values, str_replace(",","','",urldecode($_GET["provincia"])));
         }
     }
 
     if(isset($_GET["comune"])){
         if($_GET["comune"]!="") {
-            array_push($params, " id_town  = in(?)");
+            array_push($params, " id_town   in(?)");
             array_push($values, str_replace(",","','",urldecode($_GET["comune"])));
         }
     }
 
     if(isset($_GET["zona"])){
         if($_GET["zona"]!="") {
-            array_push($params, " id_district  = in(?)");
+            array_push($params, " id_district   in(?)");
             array_push($values, str_replace(",","','",urldecode($_GET["zona"])));
         }
     }
@@ -100,9 +100,11 @@
     // stato dell annuncio (non geografico)
     if(isset($_GET["stato"])){
         if($_GET["stato"]!="") {
-            array_push($params, " id_ads_status  = in(?)");
+            array_push($params, " id_ads_status   in(?)");
             array_push($values, str_replace(",","','",urldecode($_GET["stato"])));
         }
+    }else{
+        array_push($params, " id_ads_status   in(1,2)");//ENABLED,DISABLED
     }
 
     $res = $propertyM->readAllAds($params,null,$values);
@@ -120,16 +122,16 @@
 				$first_col = "<a href='AR_immobili_inserimento.php?idimmobile=".$res[$i]["id"]."' > <img class='real_tumb' src='?id=".$rand_num."' /> </a>";
 
 				/* ------------ RECUPERO DATI PER COLONNA STATO ------------ */
-				$strstato = $res[$i]["id_ads_status"]==3?"../images/ann_attivo.png":"../images/ann_bloccato.png";
+				$strstato = $res[$i]["id_ads_status"]==3?SITE_URL."/AdminPanel/images/icons/ico_ads_on.png":SITE_URL."/AdminPanel/images/icons/ico_ads_off.png";
 
 
 				$ico_trattativa="";
 				if($res[$i]["price"]=="0"){
-					$ico_trattativa="<img style='width:28px;height:14px; border:0;' title='In trattativa' src='../images/img_AR/ico_in_trattativa.png'>";
+					$ico_trattativa="<img style='width:28px;height:14px; border:0;' title='In trattativa' src='".SITE_URL."/AdminPanel/images/icons/ico_in_trattativa.png'>";
 				}elseif($res[$i]['id_contract_status']=="2"){//se venduto
-					$ico_trattativa="<img style='width:14px;height:14px; border:0;' title='Venduto' src='../images/img_AR/ico_venduto.png'>";
+					$ico_trattativa="<img style='width:14px;height:14px; border:0;' title='Venduto' src='".SITE_URL."/AdminPanel/images/icons/ico_venduto.png'>";
 				}elseif($res[$i]['id_contract_status']=="4"){// se affittato
-					$ico_trattativa="<img style='width:14px;height:14px; border:0;' title='Affittato' src='../images/img_AR/ico_affittato.png'>";
+					$ico_trattativa="<img style='width:14px;height:14px; border:0;' title='Affittato' src='".SITE_URL."/AdminPanel/images/icons/ico_affittato.png'>";
 				}
 
 				$stato_annuncio = '<div id="stato_annuncio'.$res[$i]["id"].'"><a href="javascript:SwitchAdsStatus('.$res[$i]["id"].')"><img id="ads_status_img_'.$res[$i]["id"].'" style="width:24px;height:24px;border:0px;margin-top:3px;" title="clicca per modificare" src="'.$strstato.'" ></a><p>'.$ico_trattativa.'</p></div>';
@@ -137,24 +139,24 @@
 
 				/* ------------ RECUPERO DATI PER COLONNA RIVISTA ------------ */
 				if($res[$i]["show_on_magazine"]==1){
-					$strRivista = "../images/img_AR/annuncio_in_rivista.png";
+					$strRivista = SITE_URL."/AdminPanel/images/icons/ico_newspaper_on.png";
 				}
 				else
 				{
-					$strRivista = "../images/img_AR/annuncio_nn_rivista.png";
+					$strRivista = SITE_URL."/AdminPanel/images/icons/ico_newspaper_off.png";
 				}
 				$rivista = '<a href="javascript:SwitchNewsStatus('.$res[$i]["id"].')"><img id="news_status_img_'.$res[$i]["id"].'" style="width:48px;height:48px;border:0px;margin-top:3px;" title="clicca per modificare" src="'.$strRivista.'" ></a>';
 				/* -----------   ------------ */
 
 				if($userLogged->id_user_type=="1"){
-				//controllo se l annuncio è sui portali per settare l immagine giusta, per fare ci� controllo se nella tabella immobili_portali c è almeno un portale settato a 1, se c � n � uno settato a 1 allora l immagine sarà mostra in portali, altrimenti sar� non in portali
+				//controllo se l annuncio è sui portali per settare l immagine giusta, per fare ci� controllo se nella tabella immobili_portali c è almeno un portale settato a 1, se c � n � uno settato a 1 allora l immagine sarà mostra in portali, altrimenti sarà non in portali
 					$trovato= 1;//controlloImmobiliSuiPortali($res['id'],$cn);
 					if($res[$i]["show_on_portal"]=="1"){
-						$strPortali = "../images/img_AR/mostra_in_portali.png";
+						$strPortali = SITE_URL."/AdminPanel/images/icons/ico_portal_on.png";
 					}
 					else
 					{
-						$strPortali = "../images/img_AR/non_in_portali.png";
+						$strPortali = SITE_URL."/AdminPanel/images/icons/ico_portal_off.png";
 					}
 
 					$portali = '<a href="javascript:SwitchPortalStatus('.$res[$i]["id"].')"><img id="portal_status_img_'.$res[$i]["id"].'" style="width:40px;height:40px;border:0px;margin-top:3px;" title="clicca per modificare" src="'.$strPortali.'" ></a>';
