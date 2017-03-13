42b20d3f21cbe0814e925580b30e98fadb8cff12
Fabrizio1990 <fabriziocoppolecchia@gmail.com>
2017-03-13 17:40:19 +0100

    Generalized script

--- AdminPanel/js/image_loader.js
+++ AdminPanel/js/image_loader.js
@@ -2,12 +2,12 @@
  * Created by Developer on 11/01/2017.
  */
 
-$(function () {
+/*$(function () {
     $(".file_explorer").change(function(e) {
         var elem = e.target;
         saveImage(elem);
     });
-});
+});*/
 
 
 function loadImage(image_path,elem){
@@ -15,13 +15,11 @@ function loadImage(image_path,elem){
 }
 
 
-function saveImage(elem){
+function saveImage(elem,url,imgField){
     var jqElem = $(elem);
-    url = BASE_PATH+"/AdminPanel/ajax/add_property_saveImage.ajax.php";
+    url = BASE_PATH+url;
     var imgName = jqElem.parent().children(".hidden_img_name")[0].value;
     if(imgName != undefined && imgName!= null && imgName!="")
         url+="?img_name="+imgName;
-    var imgField = jqElem.parent().parent().parent().prev().children("img")[0];
-    //console.log(elem[0].files[0]);
     SEND_INP_FILES(elem,url,function(resp){loadImage(resp,imgField)});
 }
