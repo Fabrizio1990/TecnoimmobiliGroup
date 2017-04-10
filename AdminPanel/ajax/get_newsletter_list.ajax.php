cfb9a5e67d2beaf9496b498c6d92f82c3594839a
Fabrizio1990 <fabriziocoppolecchia@gmail.com>
2017-04-10 17:52:32 +0200

    - Starting Newsletter Implementation - Various fix

---
+++ AdminPanel/ajax/get_newsletter_list.ajax.php
@@ -0,0 +1,61 @@
+<?php
+
+
+header('Content-type: text/json; charset=utf-8');
+include("../../config.php");
+/*include(BASE_PATH."/app/classes/UserManager.php");
+include(BASE_PATH."/app/classes/AgencyManager.php");*/
+
+
+$agencyMng = new AgencyManager();
+$array = array("aaData"=>array());//sintassi di base che si aspetta datatable , cioè un json con elemento padre aaData e i sottoelementi contengono i dati
+
+
+
+/*$fields = Array("id","logo_path","name","region","town","street","street_num","description","id_status","status_icon","date_ins","date_up");
+$res = $agencyMng->getAgenciesData("id_status<>3",null,null,$fields,false);
+
+$resultFound = Count($res);
+if ($resultFound>0 && $resultFound!="" && $resultFound!=null){
+    for($i=0;$i<$resultFound;$i++) {
+        $resImg = "";
+
+
+        // COLUM LOGO
+
+        // Description of agency (showed if hover on logo "img title")
+        $description = htmlentities($res[$i]["description"], ENT_QUOTES);
+        $description = $description ==""?"Nessuna descrizione":$description;
+
+        $imgPath = SITE_URL."/public/images/images_agencies_icons/min/".$res[$i]["logo_path"];
+        $first_col = "
+            <form name='goToAgency' id='goToAgency' method='POST' ACTION='".SITE_URL."/AdminPanel/agency_add.php'>
+            <input type='hidden' name='id_agency' value='".$res[$i]["id"]."'>
+            <img onclick='this.parentNode.submit()' class='real_tumb POINTER agency_logo' title='".$description."' src=".$imgPath."?id=". $rand_num ."?id=". $rand_num . "' /> </form>";
+
+
+        // COLUMN NAME
+        $name = $res[$i]["name"];
+        // COLUMN REGION
+        $region = $res[$i]["region"];
+        // COLUMN CITY
+        $city = $res[$i]["town"];
+        // COLUMN STREET
+        $street = $res[$i]["street"].", ".$res[$i]["street_num"];
+        // COLUMN STATUS
+        $status = "<input type='checkbox' class='switch' " .($res[$i]["id_status"]=="1"?"checked":"") .">";
+        // COLUMN DATE INS
+        $date_ins = Date("d-m-Y", strtotime($res[$i]["date_ins"]));
+        // COLUMN DATE UP
+        $date_up = $res[$i]["date_up"]!=""?Date("d-m-Y", strtotime($res[$i]["date_up"])):$date_ins;
+
+        array_push($array["aaData"],array($first_col,$name,$region,$city,$street,$status,$date_ins,$date_up));
+
+    }
+}*/
+
+echo(json_encode($array));
+
+
+
+?>
