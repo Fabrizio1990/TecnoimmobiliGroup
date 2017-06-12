ccbed3c8f9e043c03b5cd8d56ec5cee2fbead7c2
Fabrizio1990 <fabriziocoppolecchia@gmail.com>
2017-06-12 13:45:02 +0200

    added print magazine buttons

--- AdminPanel/include/contents/magazine_print_selection.inc.php
+++ AdminPanel/include/contents/magazine_print_selection.inc.php
@@ -12,18 +12,40 @@
             <tr>
                 <td><img src="<?php echo SITE_URL."/AdminPanel/images/rivista_completa.png"?>"</td>
                 <td>Rivista completa</td>
-                <td>Stampa</td>
+                <td>
+                    <form name="full_magazine_form" method="post" action="<?php echo SITE_URL."/rivista/stampa_rivista.html" ?>" >
+                        <button  type="button" class="btn btn-primary" id="btn_editor_submit"  onclick="this.parentNode.submit()">Stampa</button>
+
+                    </form>
+                </td>
 
             </tr>
             <tr>
                 <td><img src="<?php echo SITE_URL."/AdminPanel/images/rivista_prestige.png"?>"</td>
                 <td>rivista Prestige</td>
-                <td>Stampa</td>
+                <td>
+                    <form name="prestige_magazine_form" method="post" action="<?php echo SITE_URL."/rivista/stampa_rivista.html" ?>" >
+                        <input  type="hidden" name="id_agency" value="<?php echo $agency_id ?>">
+                        <input  type="hidden" name="magazine_type" value="prestige">
+
+                        <button  type="button" class="btn btn-primary" id="btn_editor_submit"  onclick="this.parentNode.submit()">Stampa</button>
+
+                    </form>
+                </td>
             </tr>
             <tr>
                 <td><img src="<?php echo SITE_URL."/AdminPanel/images/rivista_imprese.png"?>"</td>
                 <td>Rivista Imprese</td>
-                <td>Stampa</td>
+                <td>
+                    <form name="companies_magazine_form" method="post" action="<?php echo SITE_URL."/rivista/stampa_rivista.html" ?>" >
+                        <input  type="hidden" name="id_agency" value="<?php echo $agency_id ?>">
+                        <input  type="hidden" name="magazine_type" value="companies">
+
+                        <button  type="button" class="btn btn-primary" id="btn_editor_submit"  onclick="this.parentNode.submit()">Stampa</button>
+
+                    </form>
+
+                </td>
             </tr>
             <tr>
                 <td><img src="<?php echo SITE_URL."/AdminPanel/images/personalizza_rivista.png"?>"</td>
@@ -31,7 +53,7 @@
                 <td>
                     <form name="custom_magazine_form" method="post" action="<?php echo SITE_URL."/rivista/stampa_rivista_personalizzata.html" ?>" >
                         <input  type="hidden" name="id_agency" value="<?php echo $agency_id ?>">
-                        <button  type="button" class="btn btn-primary" id="btn_editor_submit"  onclick="document.custom_magazine_form.submit()">Stampa</button>
+                        <button  type="button" class="btn btn-primary" id="btn_editor_submit"  onclick="this.parentNode.submit()">Stampa</button>
 
                     </form>
 
