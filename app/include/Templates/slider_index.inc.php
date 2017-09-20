94084f403d9f093de324f4046e621018b552337b
Fabrizio Coppolecchia <fabriziocoppolecchia@gmail.com>
2017-09-20 12:20:22 +0200

    Initial import FRONTEND INDEX

---
+++ app/include/Templates/slider_index.inc.php
@@ -0,0 +1,58 @@
+<section id="one-parallax" class="parallax" style="background-image: url('<?php echo(SITE_URL) ?>/images/01_parallax.jpg');" data-stellar-background-ratio="0.6" data-stellar-vertical-offset="20">
+    <div class="mapandslider">
+        <div class="overlay1 dm-shadow">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-8 col-sm-12">
+                        <div id="property-slider" class="clearfix">
+                            <div class="flexslider">
+                                <ul class="slides">
+                                    <li>
+                                        <div class="desc">
+                                            <div class="ps-desc">
+                                                <h3><a href="#">Sea view duplex villa</a></h3>
+                                                <p>Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free. <a href="#">read more</a></p>
+                                                <span class="type">Villa</span>
+                                                <span class="price">€3 200 000</span>
+                                                <a href="#" class="status">On Sale</a>
+                                            </div>
+                                        </div>
+                                        <a href="#"><img src="<?php echo(SITE_URL) ?>/images/sliders/search_slider/02_slider.png" alt=""></a>
+                                    </li>
+                                    <li>
+                                        <div class="desc">
+                                            <div class="ps-desc">
+                                                <h3><a href="#">Home of your dreams</a></h3>
+                                                <p>Model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free. <a href="#">read more</a></p>
+                                                <span class="type">Family Home</span>
+                                                <span class="price">€1 112 000</span>
+                                                <a href="#" class="status">On Sale</a>
+                                            </div>
+                                        </div>
+                                        <a href="#"><img src="<?php echo(SITE_URL) ?>/images/sliders/search_slider/01_slider.png" alt=""></a>
+                                    </li>
+                                    <li>
+                                        <div class="desc">
+                                            <div class="ps-desc">
+                                                <h3><a href="#">Modern house interior</a></h3>
+                                                <p>Lo generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free. <a href="#">read more</a></p>
+                                                <span class="type">Family Home</span>
+                                                <span class="price">€1 300</span>
+                                                <a href="#" class="status">On Rent</a>
+                                            </div>
+                                        </div>
+                                        <a href="#"><img src="<?php echo(SITE_URL) ?>/images/sliders/search_slider/03_slider.png" alt=""></a>
+                                    </li>
+                                </ul><!-- end slides -->
+                            </div><!-- end flexslider -->
+                        </div><!-- end property-slider -->
+                    </div><!-- end col-lg-8 -->
+
+                    <!-- ######## HEADER contenente Logo, social buttons e menu########-->
+                    <?php include(BASE_PATH . "/app/include/Templates/research_panel_1.inc.php") ?>
+
+                </div><!-- end row -->
+            </div><!-- end dm_container -->
+        </div>
+    </div>
+</section><!-- end mapandslider -->
